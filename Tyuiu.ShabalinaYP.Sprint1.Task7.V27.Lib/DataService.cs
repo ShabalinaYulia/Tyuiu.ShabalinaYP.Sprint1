0d563a0e85bbdeddac684d8cfe8e9c75a0e4ff6d
ShabalinaYulia <julia07.kurash@gmail.com>
2024-10-02 22:26:56 +0500

    Task7

--- Tyuiu.ShabalinaYP.Sprint1.Task7.V27.Lib/DataService.cs
+++ Tyuiu.ShabalinaYP.Sprint1.Task7.V27.Lib/DataService.cs
@@ -4,9 +4,7 @@ namespace Tyuiu.ShabalinaYP.Sprint1.Task7.V27.Lib
     public class DataService : ISprint1Task7V27
     {
         public double Calculate(double x, double y)
-        {
-            x = 1; 
-            y = -1;
+        { 
             double z = (Math.Pow(Math.Cos(x), 2) + Math.Pow(Math.Sin(y), 2)) / (Math.Sin(y) + 1) - ((x * y - 12) / (15 + Math.Cos(x)));
             return Math.Round(z, 3);
         }
